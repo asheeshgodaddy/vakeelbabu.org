a80b4a727487ae0379a4f3c9efc47e22b06d9f2b
asheeshgodaddy <asheesh.tiwari@gmail.com>
2025-07-30 20:16:57 +0530

    Update g_trans.js

--- src/g_trans.js
+++ src/g_trans.js
@@ -1,7 +1,7 @@
 function googleTranslateElementInit() {
   new google.translate.TranslateElement({
     pageLanguage: 'en',
-    includedLanguages: 'en,hi,bn,fr,de,ta,te,ur,kn,gu', // customize the languages
+  includedLanguages: 'en,hi,as,bn,brx,doi,gu,kn,ks,kok,mai,ml,mni,mr,ne,or,pa,sa,sat,sd,ta,te,ur,fr,de,', // customize the languages
     layout: google.translate.TranslateElement.InlineLayout.SIMPLE
   }, 'google_translate_element');
 }
